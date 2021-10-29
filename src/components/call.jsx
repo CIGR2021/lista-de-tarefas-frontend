6f46334a84cc7916f46793149c00082c25a1cfba
CIGR2021 <appencigr@gmail.com>
2021-10-29 19:26:55 -0300

    Implementação da estrutura necessária dos componentes: call/e-mail/task.jsx

---
+++ src/components/call.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+
+function Call() {
+    return (
+        <main>
+            <header>
+                <h1>Ligações a fazer</h1>
+            </header>
+            <ul>
+                <li>(77) 9-9999-9999</li>
+            </ul>
+        </main>
+    );
+}
+
+export default Call;
