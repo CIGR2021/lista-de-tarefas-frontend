dc7fdb85a598537cf163b23088f929c026785509
CIGR2021 <appencigr@gmail.com>
2021-10-29 20:10:33 -0300

    Removendo useContext dos componentes, pois ainda não estou usando!

--- src/components/call.jsx
+++ src/components/call.jsx
@@ -1,16 +1,16 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 function Call() {
-    return (
-        <main>
-            <header>
-                <h1>Ligações a fazer</h1>
-            </header>
-            <ul>
-                <li>(77) 9-9999-9999</li>
-            </ul>
-        </main>
-    );
+  return (
+    <main>
+      <header>
+        <h1>Ligações a fazer</h1>
+      </header>
+      <ul>
+        <li>(77) 9-9999-9999</li>
+      </ul>
+    </main>
+  );
 }
 
 export default Call;
